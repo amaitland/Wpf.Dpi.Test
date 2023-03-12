e86270ce8f37e9ba55e86c4b608b38d674673fa8
Alex Maitland <amaitland@users.noreply.github.com>
2023-03-13 06:51:43 +1000

    WPF - DPI Not set via app.manifest for .Net 6.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Wpf.Dpi.Test
+
+The example targets `.Net 4.8.1`, `.Net 6.0 Windows` and `.Net 7.0 Windows`.
+
+DPI Awareness is set via `app.manifest`
+
+When targeting `.Net 6.0` the application remains `System Aware` rather than the expected `PerMonitorV2`.
+For the other targets the manifest is set correctly.
+
+Check `DPI Awareness` via `Task Manager`.
+
+```
+dotnet --list-sdks
+7.0.201 [C:\Program Files\dotnet\sdk]
+```
---
+++ Wpf.Dpi.Test/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using PInvoke;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Wpf.Dpi.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += OnWindowLoaded;
+
+            NetRuntimeVersion.Text = Environment.Version.ToString();
+
+            var processHandle = Process.GetCurrentProcess().Handle;
+
+            var result = SHCore.GetProcessDpiAwareness(processHandle, out var dpiAwareness);
+
+            ProcessDpiAwareness.Text = result == 0 ? dpiAwareness.ToString() : "Failed";
+        }
+
+        //#define DPI_AWARENESS_CONTEXT_UNAWARE              ((DPI_AWARENESS_CONTEXT)-1)
+        //#define DPI_AWARENESS_CONTEXT_SYSTEM_AWARE         ((DPI_AWARENESS_CONTEXT)-2)
+        //#define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE    ((DPI_AWARENESS_CONTEXT)-3)
+        //#define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 ((DPI_AWARENESS_CONTEXT)-4)
+        //#define DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED    ((DPI_AWARENESS_CONTEXT)-5)
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            var windowHandle = new WindowInteropHelper(this).Handle;
+
+            var ctx = User32.GetWindowDpiAwarenessContext(windowHandle);
+
+            if (User32.AreDpiAwarenessContextsEqual(ctx, new IntPtr(-1)))
+            {
+                DpiAwarenessContext.Text = "DPI_AWARENESS_CONTEXT_UNAWARE";
+            }
+            else if (User32.AreDpiAwarenessContextsEqual(ctx, new IntPtr(-2)))
+            {
+                DpiAwarenessContext.Text = "DPI_AWARENESS_CONTEXT_SYSTEM_AWARE";
+            }
+            else if (User32.AreDpiAwarenessContextsEqual(ctx, new IntPtr(-3)))
+            {
+                DpiAwarenessContext.Text = "DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE";
+            }
+            else if (User32.AreDpiAwarenessContextsEqual(ctx, new IntPtr(-4)))
+            {
+                DpiAwarenessContext.Text = "DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2";
+            }
+            else if (User32.AreDpiAwarenessContextsEqual(ctx, new IntPtr(-5)))
+            {
+                DpiAwarenessContext.Text = "DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED";
+            }
+        }
+    }
+}
